2344fe76f207c08b6694ad7091569d91b74179ca
Giulio Cipriani <cipriani.giulio.7@gmail.com>
2022-11-17 13:41:40 +0100

    Modified create_splits

--- create_splits.py
+++ create_splits.py
@@ -2,6 +2,7 @@ import argparse
 import glob
 import os
 import random
+import shutil
 
 import numpy as np
 
@@ -17,10 +18,32 @@ def split(source, destination):
         - source [str]: source data directory, contains the processed tf records
         - destination [str]: destination data directory, contains 3 sub folders: train / val / test
     """
-    # TODO: Implement function
+    # Load the data files paths using glob and shuffle them
+    files = glob.glob(f'{source}/*.tfrecord')
+    random.shuffle(files)
+
+     # Get the size of the list of files and create the split sizes: 80/10/10
+    n_files = len(files)
+    train_len = n_files*80//100
+    val_len = n_files*10//100
+
+    # Split the list of data paths into the 3 sections
+    train,val,test = files[:train_len], files[train_len:train_len+val_len],files[train_len+val_len:]
+    
+    # After creating the splits, we will copy the file (img) to the dedicated folder
+    for img in train:
+        shutil.copy(img,f'{destination}/train')
+
+    for img in val:
+        shutil.copy(img,f'{destination}/val')
+    
+    for img in test:
+        shutil.copy(img,f'{destination}/test')
+    
 
 
-if __name__ == "__main__":
+    
+if  __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
     parser.add_argument('--source', required=True,
                         help='source data directory')
